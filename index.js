bb7a157430462b8065f31f92f8da5664edcdede6
thachhn3 <thachhn3@vng.com.vn>
2021-09-16 12:12:31 +0700

    udpate music

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 const axios = require('axios').default;
 const cheerio = require('cheerio');
-const ytdl = require("ytdl-core");
+const { RepeatMode } = require('discord-music-player');
 const from = ['Phương. Nguyễn Ngọc Quỳnh (2)', 'Thạch. Huỳnh Ngọc (3)','Phụng. Tô Hoàng']
 const config = {
     authentication: process.env.msteam_token,
@@ -18,7 +18,6 @@ const options = {
 
 const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
-const queue = new Map();
 const prefix = "#";
 var textChannelId = ''
 
@@ -51,14 +50,18 @@ client.on("message", message => {
   const args = commandBody.split(' ');
   const command = args.shift().toLowerCase();
   let reply = ''
-  const serverQueue = queue.get(message.guild.id);
 
   switch (command) {
     case 'mission':
       reply = ':duck: Quack quack...! Tao là vit, con trai đầu lòng thằng Farmer'
       break;
     case 'play':
-      execute(message, serverQueue);
+      let queue = client.player.createQueue(message.guild.id);
+        await queue.join(message.member.voice.channel);
+      let song = await queue.play(args.join(' ')).catch(_ => {
+          if(!guildQueue)
+          queue.stop();
+      });
       return;
     default:
       reply = ':duck: Dm gõ tào lao'
@@ -113,82 +116,7 @@ function sendMessage(channelId, mess) {
   client.channels.cache.get(channelId).send(mess);    
 }
 // music bot
-async function execute(message, serverQueue) {
-  const args = message.content.split(" ");
 
-  const voiceChannel = message.member.voice.channel;
-  console.log("voice channel", voiceChannel)
-  if (!voiceChannel)
-    return message.channel.send(
-      ":duck: mày ở đâu mà đòi tao hát"
-    );
-  const permissions = voiceChannel.permissionsFor(message.client.user);
-  if (!permissions.has("CONNECT") || !permissions.has("SPEAK")) {
-    return message.channel.send(
-      ":duck: bye"
-    );
-  }
-
-  // const songInfo = await ytdl.getInfo(args[1]);
-
-    try {
-      var songInfo = await ytdl.getInfo(args[1]);
-    } catch (err) {
-      return message.channel.send('...');
-    }
-
-  console.log("songinfo", songInfo)
-  const song = {
-        title: songInfo.videoDetails.title,
-        url: songInfo.videoDetails.video_url,
-   };
-
-  if (!serverQueue) {
-    const queueContruct = {
-      textChannel: message.channel,
-      voiceChannel: voiceChannel,
-      connection: null,
-      songs: [],
-      volume: 5,
-      playing: true
-    };
-
-    queue.set(message.guild.id, queueContruct);
-
-    queueContruct.songs.push(song);
-
-    try {
-      var connection = await voiceChannel.join();
-      queueContruct.connection = connection;
-      play(message.guild, queueContruct.songs[0]);
-    } catch (err) {
-      console.log(err);
-      queue.delete(message.guild.id);
-      return message.channel.send(err);
-    }
-  } else {
-    serverQueue.songs.push(song);
-    return message.channel.send(`${song.title} has been added to the queue!`);
-  }
-}
-function play(guild, song) {
-  const serverQueue = queue.get(guild.id);
-  if (!song) {
-    serverQueue.voiceChannel.leave();
-    queue.delete(guild.id);
-    return;
-  }
-
-  const dispatcher = serverQueue.connection
-    .play(ytdl(song.url))
-    .on("finish", () => {
-      serverQueue.songs.shift();
-      play(guild, serverQueue.songs[0]);
-    })
-    .on("error", error => console.error(error));
-  dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
-  serverQueue.textChannel.send(`:duck: :musical_note: **${song.title}**`);
-}
 client.login(process.env.bot_token || 'ODgxOTEyMjc3NzY5NTUyMDAz.YSzu0A.xQISDzpiCAqqTXlm3_1W1fxiSaI');
 
 
